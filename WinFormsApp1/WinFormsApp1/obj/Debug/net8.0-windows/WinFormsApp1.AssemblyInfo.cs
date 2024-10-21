c13264a6355d5da3ed6e9cdeb43a7933c62e2817
KaitoKudoKK <KaitoKudoKK@gmail.com>
2024-10-21 11:25:08 +0700

    Menghapus Form1.resx

--- WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee26f6fa97c4204e1c31cbcd73eaa46a326bb2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1afe2bf57c5c4328a996b20105c6158d20b55f15")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
